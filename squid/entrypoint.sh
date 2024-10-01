01c5ecdc49b696845604ee51493a36220ffdd9e1
EliseCastle23 <109446148+EliseCastle23@users.noreply.github.com>
2024-10-01 13:12:34 -0600

    testing

--- squid/entrypoint.sh
+++ squid/entrypoint.sh
@@ -12,24 +12,11 @@ create_cache_dir() {
     chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_CACHE_DIR}
 }
 
-apply_backward_compatibility_fixes() {
-    if [[ -f ${SQUID_SYSCONFIG_DIR}/squid.user.conf ]]; then
-        rm -rf ${SQUID_SYSCONFIG_DIR}/squid.conf
-        ln -sf ${SQUID_SYSCONFIG_DIR}/squid.user.conf ${SQUID_SYSCONFIG_DIR}/squid.conf
-    fi
-}
-
 create_log_dir
 create_cache_dir
-apply_backward_compatibility_fixes
-
-# if [[ ! -d ${SQUID_CACHE_DIR}/00 ]]; then
-#     echo "Initializing cache..."
-#     $(which squid) -N -f ${SQUID_SYSCONFIG_DIR}/squid.conf -z
-# fi
 
 
-# rm -rf ${SQUID_CACHE_DIR}/ssl_db
+rm -rf ${SQUID_CACHE_DIR}/ssl_db
 /usr/lib64/squid/security_file_certgen -c -s ${SQUID_CACHE_DIR}/ssl_db -M 4MB
 chown -R squid:squid /var/cache/squid/ssl_db
 chmod -R 755 /var/cache/squid/ssl_db
