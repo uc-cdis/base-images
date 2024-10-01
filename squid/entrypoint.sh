22be368d0b4e4e55478ba2405861f32ed1a31b6d
EliseCastle23 <109446148+EliseCastle23@users.noreply.github.com>
2024-10-01 12:56:56 -0600

    testing removing ssl_db directory before chmod and chown

--- squid/entrypoint.sh
+++ squid/entrypoint.sh
@@ -28,12 +28,11 @@ if [[ ! -d ${SQUID_CACHE_DIR}/00 ]]; then
     $(which squid) -N -f ${SQUID_SYSCONFIG_DIR}/squid.conf -z
 fi
 
-sleep 300000
 
+rm -rf ${SQUID_CACHE_DIR}/ssl_db
 /usr/lib64/squid/security_file_certgen -c -s ${SQUID_CACHE_DIR}/ssl_db -M 4MB
 chown -R squid:squid /var/cache/squid/ssl_db
 chmod -R 755 /var/cache/squid/ssl_db
 
 echo "Starting squid..."
-sleep 500000000
 exec $(which squid) -N -d 2 -f ${SQUID_SYSCONFIG_DIR}/squid.conf
